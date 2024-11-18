d348dd637f044e9b3633954c23782c58860ccb97
ConChimNon0102 <nguyenvo122005@gmail.com>
2024-11-18 11:34:13 +0700

    homepage
    thêm homepage và một số thứ...

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,7 @@
 const userRouter = require("./user");
 const productRouter = require("./product");
 const authRouter = require("./auth");
+const homeRouter = require("./home")
 function route(app) {
     app.use("/reset-password", (req, res) => {
         res.render("reset_password");
@@ -8,6 +9,7 @@ function route(app) {
     app.use("/user", userRouter);
     app.use("/product", productRouter);
     app.use("/auth", authRouter);
+    app.use("/", homeRouter);
 }
 
 module.exports = route;
