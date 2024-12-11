ded507b46739093008b1a8163cf2be79f9eaf1a2
Trần Ngọc Nhân <tranngocnhannt2004@gmail.com>
2024-12-11 20:13:22 +0700

    fix homepage3

--- src/app/home/controller.js
+++ src/app/home/controller.js
@@ -5,21 +5,21 @@ const homeController = {
         try { 
             const newArrival = await Home.getNewArrival(); 
             const recomended = await Home.getRecomended();
-            let userCheck = false;
-            if(!req.user)
-                userCheck = false;
-            else{
-                if(!req.user.id)
-                    userCheck = false;
-                else
-                userCheck = await Home.findUserId(req.user.id);
-            }
+            // let userCheck = false;
+            // if(!req.user)
+            //     userCheck = false;
+            // else{
+            //     if(!req.user.id)
+            //         userCheck = false;
+            //     else
+            //     userCheck = await Home.findUserId(req.user.id);
+            // }
             res.render("home", {
                 page_style: "/css/tailwindcss.css",
                 notAJAX: true,
                 newArrival, 
                 recomended,
-                check: userCheck
+                // check: userCheck
             });
         } catch (error) {
             console.error("Error rendering home page:", error);
