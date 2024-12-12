de1d7ff7d3d6c7473a4ab997e5074b7e7456bca1
ConChimNon0102 <nguyenvo122005@gmail.com>
2024-12-12 09:09:32 +0700

    fix return url payment

--- src/config/vnpayConfig.js
+++ src/config/vnpayConfig.js
@@ -2,7 +2,7 @@ const vnpayConfig = {
     vnp_TmnCode: 'CGI1PK93', // Mã terminal
     vnp_HashSecret: 'KBQBSSJRVOVOLR7WPO7JZ5RG31KFUI14', // Chuỗi bí mật
     vnp_Url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html', // URL VNPay
-    vnp_ReturnUrl: 'http://localhost:4000/payment/vnpay_return', // URL xử lý kết quả
+    vnp_ReturnUrl: process.env.PUBLIC_ROUTE + '/payment/vnpay_return', // URL xử lý kết quả
 };
 
 module.exports = vnpayConfig;
